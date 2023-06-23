1d37504ac780c7306a30e5c804d727749cc1bf93
GN0MI0 <103611243+SauloJose@users.noreply.github.com>
2023-06-23 15:13:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+##=================================================
+#Código PRINCIPAL DA INTERFACE DE VISÃO PIN-BOT   #
+##=================================================
+#Importando aplicação
+import sys; sys.path.append('./src/')
+from app import App
+
+
+#Criando uma instância do aplicativo
+app = App()
+
+
